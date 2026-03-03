3c53f50333fd375fd7cec6008e10cb0d9b9d2203
innocent uchenna <innocentlawman@gmail.com>
2026-03-03 13:13:43 +0100

    look more into switch case method

--- switch method/switch.js
+++ switch method/switch.js
@@ -1,24 +1,27 @@
-let day = 2;
-switch(day){
-    case 1:
-        console.log("it is monday");
-        break;
-    case 2:
-        console.log("it is tuesday")
-        break;
-    case 3:
-        console.log("it is wednesday");
-        break;
-    case 4:
-        console.log("it is thursday")
-    case 5:
-        console.log("it is firday");
-        break;
-    case 6:
-        console.log("it is satursday")
-    case 7:
-        console.log("it is sunday");
-        break;
-    default:
-        console.log("this is not a day")
+const grade = document.getElementById("grade");
+const result = document.getElementById("result");
+const submit = document.getElementById("submit")
+
+submit.onclick = function(){
+    score = Number(grade.value);
+    switch(true){
+        case score >= 80:
+            result.textContent = "you got an A";
+            break;
+        case score >= 70:
+            result.textContent= "you got a B";
+            break;
+        case score >= 66:
+            result.textContent = "you got an C";
+            break;
+        case score >= 55:
+            result.textContent= "you got a D"
+            break;
+        case score >= 45:
+            result.textContent = "you got an E";
+            break;
+        case score <= 44:
+            result.textContent= "you got an F"
+            break;
+    }
 }
