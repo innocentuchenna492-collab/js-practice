2a63c33a8d40567153d3905ef0db94dc81274358
innocent uchenna <innocentlawman@gmail.com>
2026-03-03 10:12:15 +0100

    made more practice on checkbox

--- checkbox/checkbox.js
+++ checkbox/checkbox.js
@@ -5,21 +5,46 @@ const employed = document.getElementById("employed");
 const retired = document.getElementById("retired");
 const unemployed = document.getElementById("unemployed");
 const single = document.getElementById("single");
-const marrried = document.getElementById("marrried");
+const married = document.getElementById("married");
 const engaged = document.getElementById("engaged");
-const taking_stage = document.getElementById("talking_stage");
-const not_interested = document.getElementById("not_interested");
+const talkingstage = document.getElementById("talkingstage");
+const notinterested = document.getElementById("notinterested");
 const myresult = document.getElementById("myresult");
 const myoutcome = document.getElementById("myoutcome");
 const submit = document.getElementById("submit");
-document.querySelectorAll(".occupation").forEach (r => r.checked = false);
 
-submit.onclick = function(){
+submit.onclick = function() {
     if(students.checked){
-        myresult.textContent = "you are a student"
+        myresult.textContent = "you are a student";
     }
     else if(retired.checked){
-        myresult.textContent = "you are retired"
-
+        myresult.textContent = "you are retired";
+    }
+    else if(nysc.checked){
+        myresult.textContent = "you are undergoing nysc";
+    }
+    else if(employed.checked){
+        myresult.textContent = "you are employed";
+    }
+    else if(unemployed.checked){
+        myresult.textContent = "you are unemployed";
+    }
+    else if(selfemployed.checked){
+        myresult.textContent = "you are self employed";
+    }
+    if (single.checked){
+        myoutcome.textContent = "you are still single";
+    }
+    else if (married.checked){
+        myoutcome.textContent = "you are married";
+    }
+    else if(engaged.checked){
+        myoutcome.textContent = "you are engaged";
+    }
+    else if(talkingstage.checked){
+        myoutcome.textContent = "you are in a talking stage";
+    }
+    else if(notinterested.checked){
+        myoutcome.textContent = "you are not interested";
     }
 }
