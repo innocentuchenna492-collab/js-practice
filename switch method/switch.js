3278eff810cb7a9bbca60fe85cfa3d20a7f0ef5b
innocent uchenna <innocentlawman@gmail.com>
2026-03-03 10:50:54 +0100

    using switch case method application

---
+++ switch method/switch.js
@@ -0,0 +1,24 @@
+let day = 2;
+switch(day){
+    case 1:
+        console.log("it is monday");
+        break;
+    case 2:
+        console.log("it is tuesday")
+        break;
+    case 3:
+        console.log("it is wednesday");
+        break;
+    case 4:
+        console.log("it is thursday")
+    case 5:
+        console.log("it is firday");
+        break;
+    case 6:
+        console.log("it is satursday")
+    case 7:
+        console.log("it is sunday");
+        break;
+    default:
+        console.log("this is not a day")
+}
