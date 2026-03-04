024373126e3ba72841ff2e9398d6ab966046e506
innocent uchenna <innocentlawman@gmail.com>
2026-03-04 09:40:14 +0100

    looked into useful string method

---
+++ string practice/string.js
@@ -0,0 +1,5 @@
+let username = "MOVE LABE"
+console.log(username.charAt(0));
+console.log(username.lastIndexOf("E"));
+console.log(username.length);
+console.log(username.toLowerCase(username));
---
+++ string slicing/slicing.js
@@ -0,0 +1,10 @@
+let phonenumber = "805-228-8592";
+
+phonenumber = phonenumber.replaceAll("-", "");
+console.log(phonenumber);
+
+
+let username = "      yellowstone   ";
+
+username = username.trim().charAt(0);
+console.log(username);
