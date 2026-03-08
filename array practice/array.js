01d78866a21c4c51c0c203fe6ff0e30f35c9685f
innocent uchenna <innocentlawman@gmail.com>
2026-03-08 10:06:11 +0100

    looked into array and this attributes

---
+++ array practice/array.js
@@ -0,0 +1,13 @@
+let cars = ["benz", "toyota", "lenux", "innoson", "bmw"];
+
+
+//cars.push("gwagon");
+//cars.push("hunder");
+//cars[3] = "lanbogini";
+//console.log(cars);
+//cars.every();
+
+for (let i = 0; i < cars.length; i+=2){
+    console.log(cars[i]);
+
+}
